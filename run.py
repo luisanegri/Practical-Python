868aa71d6e16b2dd0f83cf085cd8030378eea7e9
Luisa Negri <luisaanegri@gmail.com>
2018-06-18 21:42:39 +0000

    scoring/function to add user score to file

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 import os
 import json 
 from flask import Flask, redirect, render_template, request
+#import pandas as pd
 
 app = Flask(__name__)
+
+data = []
+score = 0
+riddle_index = 0
 
 
 
@@ -10,32 +15,26 @@ def add_to_file (filename, data):
     with open (filename, "a") as file:
         file.writelines(data)
 
-
 #store users in users.txt 
 def add_users (username):
     add_to_file("data/users.txt", "{0}")
 
-"""score_file_name = "data/userscore.txt"
+
+def store_user_score(username, score):
+    #user input to record the log
    
-def write_score(score_file_name, username, score):
-    scores = read_scores(score_file_name)
-    # add score
-    scores[username] = score
-    with open(score_file_name, 'w') as f:
-        serializer.dump(scores, f)
-
-
-def read_scores(score_file_name):
-    try:
-        with open(score_file_name, 'r') as f:
-            scores = serializer.load(f)
-        return scores
-    except IOError:
-        # if file does not exist - we have no scores
-        return {}
-
-"""
+    user_data =  {}
+    user_data['username'] = username
+    user_data['score'] = score
     
+    return(username, score)
+
+out = {}
+
+with open('data/usersdata.json','w') as f:
+    json.dump(out, f, indent=2)      
+
+    #print(pd.read_json('data/usersdata.json'))   
     
 #GET the username, store it in the users file
 @app.route('/', methods=["GET", "POST"])
@@ -48,43 +47,39 @@ def index():
 
 @app.route('/<username>', methods=["GET", "POST"])
 def user_game(username):
-    data = []
+    global riddle_index
+    global data
+    global score
+
     with open("data/riddle.json", "r") as json_data:
         data = json.load(json_data)
+        total_questions = len(data)
         
-    index = 0
-    score = 0
    
-    
-    def add_score_to_file(username, score):
-        add_to_file("data/userscore.txt", "({0}) - {1}\n".format(
-            username.title(),
-            score))
-        
     if request.method == "POST":
              
-        index = int(request.form["index"])
-        user_answer = request.form["message"].lower()
+        riddle_index = int(request.form["riddle_index"])
+        user_answer = request.form["response"].lower()
         
         
-        if data[index]["answer"] == user_answer:
+        if data[riddle_index]["answer"] == user_answer:
         # Print correct answer
         # Show score
         # Add score to file
         # Go to next question
-            index += 1
             score += 1
-            add_score_to_file(username, score)
+            store_user_score(username, score)
             print(score)
+            riddle_index += 1
         else:
             print("That's incorrect!")
     
     if request.method == "POST":
-        if user_answer == "mount everest" and index > 9:
+        if user_answer == "mount everest" and riddle_index > 9:
             return render_template("end.html")
  
     return render_template("game.html",
-                            username=username, data=data, i=index, score=score)
+                            username=username, data=data, riddle_index=riddle_index, score=score, total_questions=total_questions)
 
     
 
